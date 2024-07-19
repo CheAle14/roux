08bdd91cc37648da9c11a54b59ec9182a750c764
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2024-07-19 17:22:15 +0100

    add error for status

--- src/models/subreddit/mod.rs
+++ src/models/subreddit/mod.rs
@@ -189,6 +189,7 @@ impl Subreddit {
             .get(&url)
             .send()
             .await?
+            .error_for_status()?
             .json::<Submissions>()
             .await?)
     }
